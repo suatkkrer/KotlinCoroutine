434b94f18ce20f99161bfced47b297ecce69e2ca
suatkkrer <suat.kukrer@gmail.com>
2021-03-20 16:26:14 +0100

    file added

--- app/src/main/java/com/suatkkrer/kotlincoroutine/RetrofitCoroutine.kt
+++ app/src/main/java/com/suatkkrer/kotlincoroutine/RetrofitCoroutine.kt
@@ -2,3 +2,21 @@ package com.suatkkrer.kotlincoroutine
 
 // retrofit coroutine project will be placed here
 
+
+fun main(){
+    val arrayname = arrayOf<Int>(1, 2, 3, 4, 5)
+
+    println(solveMeFirst(arrayname))
+}
+
+fun solveMeFirst(ar: Array<Int>): Int {
+
+    var sum = 0
+    for (i in ar) {
+        sum += ar[i]
+    }
+
+    return sum
+
+}
+
